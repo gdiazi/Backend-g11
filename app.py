5d8223810d8d5e7589e4ba611f9c6b3ecfd7314f
gdiazi <guillermo.diazipa@gmail.com>
2023-03-15 20:19:51 -0500

    cambios del profe

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
+from flask_cors import CORS
 
 from controllers.usuario_controller import UsuariosController, LoginController, PerfilController
-from controllers.tarea_controller import TareasController, ConsultaController
-
+from controllers.tarea_controller import TareasController, TareaController
 from bd import conexion
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:q1w2e3r4@localhost:5433/tareas'
@@ -16,6 +17,7 @@ app.config['JWT_SECRET_KEY'] = 'ultrasupersecreto'
 
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1,minutes=10)
 
+CORS(app, origins=['http://localhost:5173'])
 api = Api(app)
 
 conexion.init_app(app)
@@ -27,8 +29,7 @@ api.add_resource(UsuariosController, '/registro')
 api.add_resource(LoginController, '/login')
 api.add_resource(PerfilController, '/perfil')
 api.add_resource(TareasController, '/tareas')
-
-api.add_resource(ConsultaController, '/tarea', endpoint='tarea')
+api.add_resource(TareaController, '/buscar-tarea')
 
 if __name__ == '__main__':
     app.run(debug=True)
